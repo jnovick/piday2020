9fb3afc95f3560d340a476b0265fddbc57f3a569
Joshua Novick <novickjd@gmail.com>
2020-03-06 09:19:18 -0500

    Allow setting precision from browser
    * Convert App.js to use a class instead of function for the App
    component
    * Default element precision is 3 (for now)

--- src/App.js
+++ src/App.js
@@ -4,28 +4,41 @@ import GregoryLeibniz from './approximations/GregoryLeibniz'
 import Nilakantha from './approximations/Nilakantha'
 import './App.css';
 
-const options= {
+const options = {
   tex2jax: {
-      inlineMath: []
+    inlineMath: []
   },
   CommonHTML: { linebreaks: { automatic: true } },
   "HTML-CSS": { linebreaks: { automatic: true } },
-  SVG: { linebreaks: { automatic: true } },   
+  SVG: { linebreaks: { automatic: true } },
   showMathMenu: false,
   showMathMenuMSIE: false
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <MathJax.Provider options={options}>
-          <GregoryLeibniz precision={10} />
-          <Nilakantha precision={10} />
-        </MathJax.Provider>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    precision: 3
+  }
+
+  updatePrecision(event) {
+    this.setState({ precision: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <label>
+            Elements of precision: <input type="number" value={this.state.precision} onChange={this.updatePrecision.bind(this)} />
+          </label>
+          <MathJax.Provider options={options}>
+            <GregoryLeibniz precision={this.state.precision} />
+            <Nilakantha precision={this.state.precision} />
+          </MathJax.Provider>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
