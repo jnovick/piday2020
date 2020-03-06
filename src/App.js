93106477c93f992b7196b87177d03e7bab28db47
Joshua Novick <novickjd@gmail.com>
2020-03-06 08:42:37 -0500

    Add MathJax for React
    * This allows adding Latex formatted strings to show complicated math
    equations is a well formatted manner
    * Add sample for myself for the start of Gregory-Leibniz approximation
    of pi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import MathJax from 'react-mathjax';
 import './App.css';
+
+const options= {
+  tex2jax: {
+      inlineMath: []
+  },
+  CommonHTML: { linebreaks: { automatic: true } },
+  "HTML-CSS": { linebreaks: { automatic: true } },
+  SVG: { linebreaks: { automatic: true } },   
+  showMathMenu: false,
+  showMathMenuMSIE: false
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MathJax.Provider options={options}>
+          <MathJax.Node formula={"4 \\left(\\frac{1}{1} - \\frac{1}{3}\\right)"} />
+        </MathJax.Provider>
       </header>
     </div>
   );
